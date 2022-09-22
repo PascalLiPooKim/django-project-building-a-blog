51e6f90aec40c62c817d1f7a3a384973f2fec84c
PascalLiPooKim <paswei98@gmail.com>
2022-09-22 21:29:51 +0100

    fetch posts for starting page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,73 +1,8 @@
 from django.shortcuts import render
 from datetime import date
+from .models import Post
 
 all_posts = [
-    {
-        "slug": "tour-in-scotland",
-        "image": "edinburgh.jpg",
-        "author": "Pascal",
-        "date": date(2022, 9, 18),
-        "title": "Scotland Tour",
-        "excerpt": "There's nothing like the views when touring in the mountains in Scotland! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content":"""
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                Ipsa consequatur obcaecati dolorem fuga harum molestiae 
-                dignissimos architecto quasi soluta illum quam sint delectus deserunt, 
-                laboriosam quisquam praesentium. Quia, corrupti excepturi.
-                
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                Ipsa consequatur obcaecati dolorem fuga harum molestiae 
-                dignissimos architecto quasi soluta illum quam sint delectus deserunt, 
-                laboriosam quisquam praesentium. Quia, corrupti excepturi.
-                
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                Ipsa consequatur obcaecati dolorem fuga harum molestiae 
-                dignissimos architecto quasi soluta illum quam sint delectus deserunt, 
-                laboriosam quisquam praesentium. Quia, corrupti excepturi.
-            """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Pascal",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Pascal",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
     
 ]  # type: ignore
 
@@ -77,8 +12,7 @@ def get_date(post):
 # Create your views here.
 
 def startingPage(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
