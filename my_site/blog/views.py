700803d71d2dc40f9c55b78308ca443734b254e6
PascalLiPooKim <paswei98@gmail.com>
2022-09-25 22:23:06 +0100

    display comments on single post

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -51,7 +51,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
             }
         return render(request, 'blog/post-detail.html', context)
         
@@ -72,6 +73,7 @@ class SinglePostView(View):
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),
             }
         return render(request, "blog/post-detail.html", context)
     # def get_context_data(self, **kwargs):
