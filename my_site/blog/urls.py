268b00dea31e51f5ca09f25c3c8dfab0fe2290d5
PascalLiPooKim <paswei98@gmail.com>
2022-09-25 14:20:53 +0100

    convert function views to class based

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.startingPage, name="starting-page"),
-    path("posts/", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.postDetail, name="post-detail-page"), #/posts/my-first-post
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts/", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"), #/posts/my-first-post
 ]
